0bbc3d4d42072d15ec86736a2abd543aadeee7fd
Dener Cerantola <denercerantola3@hotmail.com>
2021-05-24 20:55:25 -0300

    Adicionar arquivos de projeto.

---
+++ TDE3-Calculadora/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace TDE3_Calculadora
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.btnMontante = new System.Windows.Forms.Button();
+            this.btnCapital = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMontante
+            // 
+            this.btnMontante.BackColor = System.Drawing.Color.Transparent;
+            this.btnMontante.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnMontante.FlatAppearance.BorderSize = 3;
+            this.btnMontante.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMontante.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.btnMontante.ForeColor = System.Drawing.Color.White;
+            this.btnMontante.Location = new System.Drawing.Point(77, 169);
+            this.btnMontante.Name = "btnMontante";
+            this.btnMontante.Size = new System.Drawing.Size(171, 64);
+            this.btnMontante.TabIndex = 0;
+            this.btnMontante.Text = "Calcular Montante";
+            this.btnMontante.UseVisualStyleBackColor = false;
+            this.btnMontante.Click += new System.EventHandler(this.btnMontante_Click);
+            this.btnMontante.MouseEnter += new System.EventHandler(this.btnMontante_MouseEnter);
+            this.btnMontante.MouseLeave += new System.EventHandler(this.btnMontante_MouseLeave);
+            // 
+            // btnCapital
+            // 
+            this.btnCapital.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCapital.BackColor = System.Drawing.Color.Transparent;
+            this.btnCapital.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnCapital.FlatAppearance.BorderSize = 3;
+            this.btnCapital.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCapital.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.btnCapital.ForeColor = System.Drawing.Color.White;
+            this.btnCapital.Location = new System.Drawing.Point(386, 169);
+            this.btnCapital.Name = "btnCapital";
+            this.btnCapital.Size = new System.Drawing.Size(171, 64);
+            this.btnCapital.TabIndex = 1;
+            this.btnCapital.Text = "Calcular Capital";
+            this.btnCapital.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(386, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calcular Tempo";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 3;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(77, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Calcular Taxa de Juros";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(174, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Calculadora de Juros Compostos";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(237, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Escolha a Operação";
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(644, 374);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCapital);
+            this.Controls.Add(this.btnMontante);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "frmMenu";
+            this.Text = "Menu Inicial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMontante;
+        private System.Windows.Forms.Button btnCapital;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TDE3-Calculadora/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TDE3_Calculadora
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMontante_Click(object sender, EventArgs e)
+        {
+            //Mensagem de Teste;
+            MessageBox.Show("Abre uma tela de calculo!");
+        }
+
+        private void btnMontante_MouseEnter(object sender, EventArgs e)
+        {
+            btnMontante.ForeColor = Color.Black;
+            btnMontante.BackColor = Color.White;
+            btnMontante.FlatAppearance.BorderColor = Color.Black;
+        }
+
+        private void btnMontante_MouseLeave(object sender, EventArgs e)
+        {
+            btnMontante.ForeColor = Color.White;
+            btnMontante.BackColor = Color.Transparent;
+            btnMontante.FlatAppearance.BorderColor = Color.White;
+        }
+    }
+}
