d69e2ee12bb383f7eafc0d8f5a9578a78527b843
Dener Cerantola <denercerantola3@hotmail.com>
2021-05-26 09:11:37 -0300

    Calculo de Capital Iniciado

--- TDE3-Calculadora/Form3.Designer.cs
+++ TDE3-Calculadora/Form3.Designer.cs
@@ -29,12 +29,175 @@ namespace TDE3_Calculadora
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMontante = new System.Windows.Forms.TextBox();
+            this.txtTaxa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTempo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.FlatAppearance.BorderSize = 3;
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.btnVoltar.ForeColor = System.Drawing.Color.White;
+            this.btnVoltar.Location = new System.Drawing.Point(12, 12);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(95, 37);
+            this.btnVoltar.TabIndex = 0;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            this.btnVoltar.MouseEnter += new System.EventHandler(this.btnVoltar_MouseEnter);
+            this.btnVoltar.MouseLeave += new System.EventHandler(this.btnVoltar_MouseLeave);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.FlatAppearance.BorderSize = 3;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(370, 312);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(95, 37);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            this.btnCalcular.MouseEnter += new System.EventHandler(this.btnCalcular_MouseEnter);
+            this.btnCalcular.MouseLeave += new System.EventHandler(this.btnCalcular_MouseLeave);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.FlatAppearance.BorderSize = 3;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(494, 312);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(95, 37);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            this.btnLimpar.MouseEnter += new System.EventHandler(this.btnLimpar_MouseEnter);
+            this.btnLimpar.MouseLeave += new System.EventHandler(this.btnLimpar_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label1.Location = new System.Drawing.Point(66, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Montante";
+            // 
+            // txtMontante
+            // 
+            this.txtMontante.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.txtMontante.Location = new System.Drawing.Point(54, 101);
+            this.txtMontante.Name = "txtMontante";
+            this.txtMontante.Size = new System.Drawing.Size(179, 26);
+            this.txtMontante.TabIndex = 4;
+            // 
+            // txtTaxa
+            // 
+            this.txtTaxa.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.txtTaxa.Location = new System.Drawing.Point(54, 170);
+            this.txtTaxa.Name = "txtTaxa";
+            this.txtTaxa.Size = new System.Drawing.Size(179, 26);
+            this.txtTaxa.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label2.Location = new System.Drawing.Point(66, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Taxa";
+            // 
+            // txtTempo
+            // 
+            this.txtTempo.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.txtTempo.Location = new System.Drawing.Point(54, 244);
+            this.txtTempo.Name = "txtTempo";
+            this.txtTempo.Size = new System.Drawing.Size(179, 26);
+            this.txtTempo.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label3.Location = new System.Drawing.Point(66, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tempo";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.txtResultado.Location = new System.Drawing.Point(370, 170);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(179, 26);
+            this.txtResultado.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label4.Location = new System.Drawing.Point(382, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Resultado";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(625, 379);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTempo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTaxa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMontante);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnVoltar);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMontante;
+        private System.Windows.Forms.TextBox txtTaxa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTempo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label4;
     }
 }
--- TDE3-Calculadora/Form3.cs
+++ TDE3-Calculadora/Form3.cs
@@ -16,5 +16,67 @@ namespace TDE3_Calculadora
         {
             InitializeComponent();
         }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnVoltar_MouseEnter(object sender, EventArgs e)
+        {
+            btnVoltar.ForeColor = Color.Black;
+            btnVoltar.BackColor = Color.White;
+            btnVoltar.FlatAppearance.BorderColor = Color.Black;
+        }
+
+        private void btnVoltar_MouseLeave(object sender, EventArgs e)
+        {
+            btnVoltar.ForeColor = Color.White;
+            btnVoltar.BackColor = Color.Transparent;
+            btnVoltar.FlatAppearance.BorderColor = Color.White;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_MouseEnter(object sender, EventArgs e)
+        {
+            btnCalcular.ForeColor = Color.Black;
+            btnCalcular.BackColor = Color.White;
+            btnCalcular.FlatAppearance.BorderColor = Color.Black;
+        }
+
+        private void btnCalcular_MouseLeave(object sender, EventArgs e)
+        {
+            btnCalcular.ForeColor = Color.White;
+            btnCalcular.BackColor = Color.Transparent;
+            btnCalcular.FlatAppearance.BorderColor = Color.White;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtMontante.Text = "";
+            txtTaxa.Text = "";
+            txtTempo.Text = "";
+            txtResultado.Text = "";
+
+            MessageBox.Show("Tudo Limpo!");
+        }
+
+        private void btnLimpar_MouseEnter(object sender, EventArgs e)
+        {
+            btnLimpar.ForeColor = Color.Black;
+            btnLimpar.BackColor = Color.White;
+            btnLimpar.FlatAppearance.BorderColor = Color.Black;
+        }
+
+        private void btnLimpar_MouseLeave(object sender, EventArgs e)
+        {
+            btnLimpar.ForeColor = Color.White;
+            btnLimpar.BackColor = Color.Transparent;
+            btnLimpar.FlatAppearance.BorderColor = Color.White;
+        }
     }
 }
